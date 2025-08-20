2187062d39f6df031312b65ccb8425b3f9cb9e21
acevancho <aevancho@me.com>
2025-08-19 19:46:29 -0500

    tonemenu frontend

--- src/renderer/src/stores/useStore.ts
+++ src/renderer/src/stores/useStore.ts
@@ -4,26 +4,28 @@ type State = {
 	isLoading: boolean
 	error: string | null
 	data: any | null
+  label: null | string
 	searchTerm: null | string
 	searchType: null | string
 	setSearchTerm: (term: string | null) => void
 	reset: () => void
-	fetchData: (searchTerm: string, searchType: string) => Promise<void>
+	fetchData: (searchTerm: string, searchType: string, label?: string) => Promise<void>
 }
 
 export const useStore = create<State>(set => ({
 	isLoading: false,
 	error: null,
 	data: null,
+  label: null,
 	searchTerm: null,
 	searchType: null,
 	setSearchTerm: (term: string | null): void => set({ searchTerm: term }),
 	reset: (): void => set({ isLoading: false, data: null, error: null }),
-	fetchData: async (searchTerm: string, searchType: string): Promise<void> => {
-		set({ isLoading: true, error: null, searchTerm, searchType })
-
+	fetchData: async (searchTerm: string, searchType: string, label?: string): Promise<void> => {
+		set({ isLoading: true, error: null, searchTerm, searchType, label })
+    searchType = searchType.trim().toLowerCase();
 		try {
-			const result = await window.api.invoke('search:thesaurus', searchTerm)
+			const result = await window.api.invoke(searchType, searchTerm)
 			set({ data: result, isLoading: false })
 		} catch (err: any) {
 			set({ error: String(err?.message ?? err), isLoading: false })
